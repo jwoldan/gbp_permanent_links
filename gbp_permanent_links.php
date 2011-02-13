ba4270ece9f8d4b358b85a396b18b3ce5b4aaef6
jwoldan <jwoldan@gmail.com>
2011-02-12 23:15:47 -0500

    Updated version number. From last commit: URLs which match more than one article now return the article with the most recent posted date instead of the article with the highest post ID.

--- gbp_permanent_links.php
+++ gbp_permanent_links.php
@@ -1,7 +1,7 @@
 <?php
 
 $plugin['name'] = 'gbp_permanent_links_jrw';
-$plugin['version'] = '0.14_jrw';
+$plugin['version'] = '0.14_jrw.01';
 $plugin['author'] = 'Graeme Porteous, modified by Jeffrey Woldan';
 $plugin['author_uri'] = 'http://rgbp.co.uk/projects/textpattern/gbp_permanent_links/';
 $plugin['description'] = 'Custom permanent links rules';
